4094558a88932abd00ff681a28da337fc486af3b
MysticalDevil <jinpeng7981@outlook.com>
2026-02-22 13:03:39 +0800

    refactor(session): extract roguelike choice flow into dedicated unit

--- src/game_logic_session.cpp
+++ src/game_logic_session.cpp
@@ -2,10 +2,8 @@
 
 #include <QDateTime>
 
-#include "adapter/choice_models.h"
 #include "adapter/level_loader.h"
 #include "adapter/profile_bridge.h"
-#include "core/choice_runtime.h"
 #include "core/game_rules.h"
 #include "fsm/game_state.h"
 #include "fsm/state_factory.h"
@@ -15,7 +13,6 @@ using namespace Qt::StringLiterals;
 namespace
 {
 constexpr int InitialInterval = 200;
-constexpr int BuffDurationTicks = 40;
 } // namespace
 
 auto GameLogic::hasSave() const -> bool
@@ -130,49 +127,6 @@ void GameLogic::lazyInitState()
             changeState(std::move(nextState));
         }
     }
-}
-
-void GameLogic::generateChoices()
-{
-    const QList<snakegb::core::ChoiceSpec> allChoices =
-        snakegb::core::pickRoguelikeChoices(m_rng.generate(), 3);
-    m_choices = snakegb::adapter::buildChoiceModel(allChoices);
-    emit choicesChanged();
-}
-
-void GameLogic::selectChoice(const int index)
-{
-    if (index < 0 || index >= m_choices.size()) {
-        return;
-    }
-
-    if (m_state != Replaying) {
-        m_currentChoiceHistory.append({.frame = m_gameTickCounter, .index = index});
-    }
-
-    const auto type = snakegb::adapter::choiceTypeAt(m_choices, index);
-    if (!type.has_value()) {
-        return;
-    }
-    m_lastRoguelikeChoiceScore = m_score;
-    m_activeBuff = static_cast<PowerUp>(type.value());
-    applyAcquiredBuffEffects(type.value(), BuffDurationTicks * 2, false, true);
-
-    emit buffChanged();
-    if (m_state == Replaying) {
-        m_timer->setInterval(normalTickIntervalMs());
-        return;
-    }
-
-    m_timer->setInterval(500);
-
-    QTimer::singleShot(500, this, [this]() -> void {
-        if (m_state == Playing) {
-            m_timer->setInterval(normalTickIntervalMs());
-        }
-    });
-
-    requestStateChange(Playing);
 }
 
 auto GameLogic::buildSafeInitialSnakeBody() const -> std::deque<QPoint>
